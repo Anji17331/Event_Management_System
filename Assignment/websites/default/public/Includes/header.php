3283fe23bb766f549b5b34bceb7edf6fff890811
Anji17331 <anjanikumar171331@gmail.com>
2025-04-15 22:43:17 +0100

    Refactor header structure and add navigation links

--- Assignment/websites/default/public/Includes/header.php
+++ Assignment/websites/default/public/Includes/header.php
@@ -1,8 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<header>
+    <div class="logo">Chronos Revel</div>
+
+    <div class="search_container">
+        <input type="text" placeholder="Search">
+    </div>
+
+    <div class="user_actions">
+        <a href="#" target="blank">
+            <img src="Icons/favorite_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg" alt="Favorites">
+        </a>
+        <a href="#" target="blank">
+            <img src="Icons/shopping_cart_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg" alt="Cart">
+        </a>
+        <a href="#" target="blank">
+            <img src="Icons/account_circle_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg" alt="Account">
+        </a>
+        <button class="login_button">Login</button>
+    </div>
+</header>
+
+<nav>
+            <a href="#">Home</a>
+            <a href="#">History</a>
+            <a href="#">Upcoming</a>
+            <a href="#">About us</a>
+</nav>
