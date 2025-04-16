7452dfe9a491320eb07fa51a2e83c7b85e2c24ab
Anji17331 <anjanikumar171331@gmail.com>
2025-04-16 12:52:14 +0100

    Add login page and styles; implement responsive design adjustments

---
+++ Assignment/websites/default/public/Auth/login.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="..\vje.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="left">
+            <h1>Evoke Limitless possibilities <br> and new experience</h1>
+        </div>
+        <div class="right">
+            <div class="card">
+                <div class="buttons">
+                    <button>USER</button>
+                    <button> ADMIN</button>
+                </div>
+                <div class="logo">Chronos Revel</div>
+                <p><strong>Welcome back</strong></p>
+                <p>Enter your Details to login</p>
+                <form action="" method="POST">
+                    <input type="text" placeholder="username" name="username" required>
+                    <input type="password" placeholder="password" name="password" required>
+                    <div class="forgot_password">
+                        <a href="#">Forgot password?</a>
+                    </div>
+                    <button type="submit">Login</button>
+                </form>
+                <div class="register_section">
+                    <p>Don't have an account? <a href="#">Register</a></p>
+                </div>
+                <div class="social_login">
+                    <p>Or</p>
+                    <p>Signup with</p>
+                    <div class="social_buttons">
+                        <button><img src="" alt="Google"></button>
+                        <button><img src="" alt="Facebook"></button>
+                        <button><img src="" alt="Twitter"></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+</body>
+
+</html>
